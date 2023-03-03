810bc9c84c56a776e499bd0e25544f0bf444b99a
nhatnt <xuanlampltn@gmail.com>
2023-03-03 16:51:41 +0700

    add package  cmd

--- main.go
+++ main.go
@@ -1,82 +1,29 @@
 package main
 
 import (
-	"context"
 	"fmt"
 	"os"
-	"server-test/api"
-	"server-test/cache"
-	"server-test/config"
-	"server-test/database/db"
-	"server-test/database/mongodb"
-	"server-test/database/mysqldb"
-
-	log "github.com/sirupsen/logrus"
+	"server-test/cmd"
 )
 
-type Author struct {
-	Name string `json:"name"`
-	Age  int    `json:"age"`
-}
-
-var (
-	version string
-)
+var version string
 
 func main() {
-	arg := os.Args[1]
+	fmt.Println(os.Args[1])
+	if len(os.Args) == 2 {
 
-	if arg == "--version" {
-		fmt.Println("Version: ", version)
-	} else {
-		config := config.GetConfig()
+		switch os.Args[1] {
 
-		switch config.Sever.TypeServer.Name {
-
-		case "server_levedb":
-			// leveldb, err := levedb.InitLeveDb(config)
-
-			// if err != nil {
-			// 	fmt.Println("Error connecting to database : error=% ", err, leveldb)
-			// }
-
-		case "server_mysql":
-			_, err := mysqldb.InitMySqlDb(config)
-			if err != nil {
-				fmt.Println("Error connecting to database : error=% ", err)
-			}
-		case "server_postgressql":
-
-			db, err := db.Init(config)
-
-			if err != nil {
-				panic(err)
-			}
-
-			err = db.Ping()
-			if err != nil {
-				panic(err)
-			}
-		case "server_mongodb":
-			mongodb.InitMongoDB(config)
+		case "version":
+			cmd.Version(version)
+		case "help":
+			cmd.Help()
+		case "run":
+			cmd.Run()
 		default:
-			fmt.Println("Don't have sever  " + config.Sever.TypeServer.Name)
+			cmd.Help()
 		}
-
-		//----------------------------------------------------------------------------//
-		fmt.Println("Successfully " + config.Sever.TypeServer.Name + " connected")
-		ctx := context.TODO()
-		cache.ConnectRedis(ctx)
-
-		values := cache.GetAllKeys(ctx, "id*")
-
-		log.Info("All values : %v \n", values)
-
-		serverAddr := "0.0.0.0:3000"
-		// serverAddrhttp := "0.0.0.0:3003"
-
-		// go api.GRPCSever(serverAddrhttp)
-		api.GatewaySever(serverAddr, config)
+	} else {
+		cmd.Help()
 	}
-
 }
